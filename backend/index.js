5eb9fef0417b688986ba543cf37cb8855895ee1e
basgomcesar <cesarbasiliogomez@gmail.com>
2025-01-19 15:33:15 -0600

    add enviroment variables

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,27 @@
-const express = require('express');
-const app = express();
+const { Sequelize } = require('sequelize');
 
-const PORT = 3000;
-
-// Middleware para parsear JSON
-app.use(express.json());
-
-// Ruta de ejemplo
-app.get('/', (req, res) => {
-    res.send('¡Bienvenido al backend en Node.js!');
+// Configuración de Sequelize
+const sequelize = new Sequelize('Certificade_GS', null, null, {
+  dialect: 'mssql',
+  dialectOptions: {
+    options: {
+      trustedConnection: true,
+    },
+  },
+  host: 'CEBAGO//SQLEXPRESS', // Cambia por tu servidor SQL
+  port: 1433, // Cambia si usas otro puerto
+  logging: console.log, // Puedes desactivarlo si no necesitas ver las consultas
+  define: {
+    freezeTableName: true, // Evita pluralizar nombres de tablas
+  },
 });
 
-// Iniciar servidor
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+// Probar la conexión
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexión establecida correctamente con Sequelize.');
+  } catch (error) {
+    console.error('No se pudo conectar a la base de datos:', error);
+  }
+})();
